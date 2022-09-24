18309df3cd4f9fcc664710276910cdc90817f95d
Jawher B <jawher_bouhouch@yahoo.com>
2022-09-24 13:41:47 +0200

    initialize the server and the app

---
+++ app.js
@@ -0,0 +1,14 @@
+const express = require('express');
+const bodyParser = require('body-parser');
+
+const app = express();
+const dotenv = require('dotenv');
+
+dotenv.config();
+
+
+app
+  .use(bodyParser.urlencoded({extended: true}))
+  .use(bodyParser.json({  extended: true }))
+
+module.exports = app;
