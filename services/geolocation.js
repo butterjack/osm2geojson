4d6ca1c1de1f9961c20d871d4618c450c22c87bf
Jawher B <jawher_bouhouch@yahoo.com>
2022-09-25 17:36:18 +0200

    add query-overpass to get geojson location

--- services/geolocation.js
+++ services/geolocation.js
@@ -1,6 +1,11 @@
 const axios = require('axios');
 const osmtogeojson = require('osmtogeojson');
+const query_overpass = require('query-overpass');
 
+// osmtogeojson package is not maintained(last publication was 3 years before)
+// and it has a lot of open issues(44 issues) 
+// Relevant issue: https://github.com/tyrasd/osmtogeojson/issues/130
+// So I worked with the package as instructed and I added the results from query-overpass 
 const getGeoJsonLocation = async (req, res, next) => {
     // get bbox from query string
     let result, bbox = req.query.bbox;
@@ -14,7 +19,21 @@ const getGeoJsonLocation = async (req, res, next) => {
 
     // convert to geojson
     const geojson_data = osmtogeojson(result.data);
-    return res.status(200).send(geojson_data)
+
+    // add query_overpass data
+    query_overpass(`node(${bbox})[amenity=bar];out;`, (
+        err,data) => {
+            if(err){
+                return res.json({osmtogeojson: geojson_data})
+            }
+            console.log(data)
+            return res.json({
+                osmtogeojson: geojson_data,
+                query_overpass: data
+            })
+        }
+    )
+    
 };
 
 module.exports = {getGeoJsonLocation};
